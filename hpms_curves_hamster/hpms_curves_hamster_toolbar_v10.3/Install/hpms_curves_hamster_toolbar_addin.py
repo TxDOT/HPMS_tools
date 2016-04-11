1a9a4e9e73ebfd4a3ebda2ed5633d8e5692cee51
Jason Kleinert <T-fal@users.noreply.github.com>
2016-04-11 08:06:23 -0500

    Update hpms_curves_hamster_toolbar_addin.py

--- hpms_curves_hamster/hpms_curves_hamster_toolbar_v10.3/Install/hpms_curves_hamster_toolbar_addin.py
+++ hpms_curves_hamster/hpms_curves_hamster_toolbar_v10.3/Install/hpms_curves_hamster_toolbar_addin.py
@@ -2,7 +2,7 @@
 The MIT License (MIT)
 
 Copyright (c) 2016 Texas Department of Transportation
-Author: Jason Kleinert
+Author: Jason Kleinert & David Hickman
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
